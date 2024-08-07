8809d41bec52a369610b997e027b36cc5acd6dae
kush <kushtejani50@gmail.com>
2024-08-07 11:59:46 +0530

    first commit

---
+++ admin/src/App.tsx
@@ -0,0 +1,8 @@
+// import { Button } from "@/components/ui/button"
+import { Dashboard } from "./components/component/dashboard"
+
+export default function Home() {
+  return (
+    <Dashboard/>
+  )
+}
